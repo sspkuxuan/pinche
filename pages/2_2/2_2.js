a669588ddf199d9c9d84c7ac4c16731f7350feb7
sspkuxuan <1801210380@pku.edu.cn>
2019-01-20 15:22:50 +0800

    first commit

---
+++ pages/2_2/2_2.js
@@ -0,0 +1,180 @@
+// pages/2_2/2_2.js
+const app = getApp()
+var dateTimePicker = require('../../utils/dateTimePicker.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+     array: ['1人', '2人', '3人'],
+    objectArray: [
+      {
+        id: 0,
+        name: '1人'
+      },
+      {
+        id: 1,
+        name: '2人'
+      },
+      {
+        id: 2,
+        name: '3人'
+      }
+    ],
+    date: '2018-10-01',
+    time: '12:00',
+    dateTimeArray: null,
+    dateTime: null,
+    dateTimeArray1: null,
+    dateTime1: null,
+    startYear: 2018,
+    endYear: 2050,
+    index: 0,
+ 
+    sex: ['1人', '2人', '3人'], 
+
+  },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.getLocation({
+      type: "wgs84",
+      success: function (res) {
+        var latitude = res.latitude;
+        var longitude = res.longitude;
+        //console.log(res.latitude);
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          markers: [{
+            latitude: res.latitude,
+            longitude: res.longitude
+          }]
+          // , covers: [{
+          //   latitude: res.latitude,
+          //   longitude: res.longitude,
+          //   iconPath: '/img/location1.png'
+          // }, {
+          //     latitude: res.latitude,
+          //     longitude: res.longitude,
+          //   iconPath: '/img/location1.png'
+          // }]
+        })
+      }
+    });
+    // 获取完整的年月日 时分秒，以及默认显示的数组
+    var obj = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    var obj1 = dateTimePicker.dateTimePicker(this.data.startYear, this.data.endYear);
+    // 精确到分的处理，将数组的秒去掉
+    var lastArray = obj1.dateTimeArray.pop();
+    var lastTime = obj1.dateTime.pop();
+
+    this.setData({
+      dateTime: obj.dateTime,
+      dateTimeArray: obj.dateTimeArray,
+      dateTimeArray1: obj1.dateTimeArray,
+      dateTime1: obj1.dateTime
+    });
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function (e) {
+    this.mapCtx = wx.createMapContext('myMap')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+   bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  open: function () {
+    this.setData({
+      condition: !this.data.condition
+    })
+  },
+ 
+  selectsex: function (e) {
+    this.setData({
+      'userInfo.gender': e.detail.value
+    })
+  },
+  changeDateTime1(e) {
+    this.setData({ dateTime1: e.detail.value });
+  },
+  changeDateTimeColumn1(e) {
+    var arr = this.data.dateTime1, dateArr = this.data.dateTimeArray1;
+
+    arr[e.detail.column] = e.detail.value;
+    dateArr[2] = dateTimePicker.getMonthDay(dateArr[0][arr[0]], dateArr[1][arr[1]]);
+
+    this.setData({
+      dateTimeArray1: dateArr,
+      dateTime1: arr
+    });
+  },
+  
+
+ 
+})
+
